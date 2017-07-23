1f86eb09a915ffbb0d7732e1f24fab4d4216b614
Noam Chadash <noam@stratoscale.com>
2017-07-23 11:52:43 +0300

    Adding automatic ec2_credentials generation

--- aws/boto_tutorial.py
+++ aws/boto_tutorial.py
@@ -14,7 +14,7 @@ def get_args():
     parser.add_argument('--domain', required=True, help='Domain used to retrieve date')
     parser.add_argument('--username', required=True, help='User name used to retrieve date')
     parser.add_argument('--password', required=True, help='Password used to retrieve date')
-    parser.add_argument('--project', default="default", help='Password used to retrieve date')
+    parser.add_argument('--project', default="default", help='Project to use')
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -23,16 +23,18 @@ if __name__ == "__main__":
     my_session.verify = False
     client = symphony_client.Client(url='https://%s' % my_args.cluster_ip, session=my_session)
     client.login(domain=my_args.domain, username=my_args.username, password=my_args.password, project=my_args.project)
-    # access = client.access.get()
-    # secret = client.secret.get()
-    access = "fe16c4d196cc4c889ff197294d962d2f"
-    secret = "3fcb3cc709ea4d8db1c185e22d5ddf8c"
+    for user in client.users.list():
+        if user['name'] == my_args.username:
+            userID = user['id']
+    for project in client.projects.list():
+        if project['name'] == my_args.project:
+            projectID = project['id']
+    ec2_credentials = client.users.create_ec2_credentials(userID, projectID)
     botoclient = boto3.Session.client(boto3.session.Session(), service_name="ec2", region_name="symphony",
                                   endpoint_url="https://%s/api/v2/ec2/" % my_args.cluster_ip,
                                   verify=False,
-                                  aws_access_key_id=access,
-                                  aws_secret_access_key=secret)
+                                  aws_access_key_id=ec2_credentials['access'],
+                                  aws_secret_access_key=ec2_credentials['secret'])
 
-    # finding our Centos image, grabbing its image ID
     images = botoclient.describe_images()
-    import ipdb; ipdb.set_trace()
+    print images
